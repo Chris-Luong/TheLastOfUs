2f7033480d11e24e587fbce7a47088733e01a666
Mikkel Endresen <z5304263@unsw.edu.au>
2023-04-11 15:32:24 +1000

    Commit for pull

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -5,6 +5,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
+from sklearn import svm
 
 
 class LogReg:
@@ -21,7 +22,7 @@ class LogReg:
         # Create a one-vs-all logistic regression model and fit it to the training data
         # Saga is recommended for larger datasets and uses a version of SGD
         lr = LogisticRegression(
-            max_iter=1000, solver="saga", multi_class='ovr')
+            max_iter=1000, solver="saga", multi_class='ovr', class_weight={0: 75, 1: 4, 2: 1})
         # , class_weight={0: 31, 1: 2, 2: 1}
         # , penalty="l2", C=0.1,
         ovr = OneVsRestClassifier(lr)
@@ -53,4 +54,16 @@ class LogReg:
         # Convert the binary predictions back to multi-class labels
         y_pred = lb.inverse_transform(y_pred_binary)
 
+        return y_pred
+
+    # TODO: Try an SVM?
+    def svm(self):
+        # Create SVM classifier with a linear kernel
+        clf = svm.SVC(kernel='linear')
+
+        # Train the classifier on the training data
+        clf.fit(self.datasets.X_train, self.datasets.y_train)
+
+        # Predict labels on the test data
+        y_pred = clf.predict(self.datasets.X_test)
         return y_pred
