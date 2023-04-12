f1c6a5c7e871254071e348273f8a988aacacfa8b
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2023-04-12 22:45:02 +1000

    increased random forest precision to 75% with hyperparameter tuning

--- models/random_forest.py
+++ models/random_forest.py
@@ -1,9 +1,22 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
+
+# These parameters increased precision to 75%
+params = {
+    "n_estimators": 80,
+    "min_samples_leaf": 31,
+    "min_samples_split": 2,
+    "max_depth": 10,
+    "max_features": 80,
+    'criterion': "gini",
+    'n_jobs': 15,
+    'random_state': 42
+}
 
 def RF(data):
     # Initialize a DecisionTreeClassifier object
-    clf = RandomForestClassifier(n_estimators=100, max_features=5)
+    # clf = RandomForestClassifier(n_estimators=100, max_features=5)
+    clf = RandomForestClassifier(**params)
 
     # Fit the classifier to the training data
     clf.fit(data.X_train, data.y_train)
